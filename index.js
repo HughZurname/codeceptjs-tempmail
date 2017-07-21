adaf003b8fd374696f79bdf43a901e59060ff2ea
Craig de Gouveia <craig.degouveia@gmail.com>
2017-07-21 09:14:15 +0100

    refactored to not use arrow functions because comedy

--- index.js
+++ index.js
@@ -10,8 +10,7 @@ class Mailbox extends Helper {
      * @param {Object} config configuration can be overridded by values found in `codecept.json
      */
     constructor(config) {
-        this.apiUrl = 'http://api.temp-mail.ru'
-        this.domains = ["@p33.org", "@binka.me", "@doanart.com"]
+        this.apiUrl = config.apiUrl || 'http://api.temp-mail.ru'
         this.mailbox = {
             address: '',
             messages: []
@@ -19,46 +18,60 @@ class Mailbox extends Helper {
     }
 
     _init() {
-        createMailbox(null)
+        return this.createMailbox(null);
     }
 
-    _hashAddress = (str) => (new Promise((resolve, reject) => (
-        str ? resolve(crypto.createHash('md5').update(str).digest('hex')) : reject("No string provided")
-    )))
+    _hashAddress(str) {
+        return new Promise((resolve, reject) => (
+            str ? resolve(crypto.createHash("md5").update(str).digest("hex")) : reject("No string provided")));
+    }
 
-    _transformResponse = (res) => (res.json())
+    _transformResponse(res) {
+        return res.json();
+    }
 
-    _fetchTransform = (path) => (fetch(`${apiUrl}${path || ''}/format/json/`).then(_transformResponse))
+    _fetchTransform(path) {
+        return fetch(`${this.apiUrl}${path || ""}/format/json/`).then(this._transformResponse);
+    }
 
-    _randArr = (arr) => (arr[Math.floor(Math.random() * arr.length)])
+    _randArr(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
+    }
 
-    _randStr = () => (Math.random().toString(32).substring(2))
+    _randStr() {
+        return Math.random().toString(32).substring(2);
+    }
 
-    _genRandomMail = () => (
-        _fetchTransform('/request/domains/')
-        .then(r => _randStr() + _randArr(r))
-        .then(a => this.mailbox.address = a)
-    )
+    _genRandomMail() {
+        return this._fetchTransform("/request/domains/")
+            .then(r => this._randStr() + this._randArr(r))
+            .then(a => this.mailbox.address = a)
+            .then(e => console.log(e));
+    }
 
-    _genNamedMail = (str = this.mailbox.address) => (
-        _fetchTransform('/request/domains/')
-        .then(r => str + _randArr(r))
-        .then(a => this.mailbox.address = a)
-    )
+    _genNamedMail(name = this.mailbox.address) {
+        return this._fetchTransform("/request/domains/")
+            .then(r => name + this._randArr(r))
+            .then(a => this.mailbox.address = a);
+    }
 
-    createMailbox = (name = this.mailbox.address) => (
-        name ? _genNamedMail(name).then(getMessages) : _genRandomMail().then(getMessages)
-    )
+    createMailbox(name = this.mailbox.address) {
+        return name ? this._genNamedMail(name).then(this.getMessages) : this._genRandomMail().then(this.getMessages);
+    }
 
-    deleteMessage = (m) => (
-        _fetchTransform(`/request/delete/id/${m}`)
-    )
+    deleteMessage(msgId) {
+        return this._fetchTransform(`/request/delete/id/${msgId}`)
+    }
 
-    getMessages = (addr = this.mailbox.address) => (
-        _hashAddress(addr)
-        .then(hash => _fetchTransform(`/request/mail/id/${hash}`))
-        .then(m => this.mailbox.messages = m)
-    )
+    getMessages(addr = this.mailbox.address) {
+        return this._hashAddress(addr)
+            .then(hash => this._fetchTransform(`/request/mail/id/${hash}`))
+            .then(m => this.mailbox.messages = m)
+    }
+
+    getMailbox() {
+        return this.mailbox
+    }
 }
 
 module.exports = Mailbox;
