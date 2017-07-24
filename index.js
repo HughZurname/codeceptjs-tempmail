0b6f7a5270f151314a1e6b92f5ff8ea470ddf05f
Craig de Gouveia <craig.degouveia@gmail.com>
2017-07-24 16:06:27 +0100

    Message handling and retries: - Added retry function - Consumed new functions in exported class for use in the codecept I object. - Finished getLatest function.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const crypto = require('crypto');
 const fetch = require('node-fetch');
+const promiseRetry = require("promise-retry");
 
 const apiUrl = 'http://api.temp-mail.ru'
 
@@ -7,9 +8,14 @@ const _hashAddress = (str) => (new Promise((resolve, reject) => (
     str ? resolve(crypto.createHash('md5').update(str).digest('hex')) : reject("No string provided")
 )))
 
-const _transformResponse = (res) => (res.json())
+const _transformResponse = (res) => (new Promise((resolve, reject) => (
+    typeof res === typeof {} ? resolve(res.json()) : reject("Invalid data type")
+)))
 
-const _fetchTransform = (path) => (fetch(`${apiUrl}${path || ''}/format/json/`).then(_transformResponse))
+const _fetchTransform = (path) => (
+    fetch(`${apiUrl}${path || ''}/format/json/`)
+    .then(_transformResponse)
+)
 
 const _randArr = (arr) => (arr[Math.floor(Math.random() * arr.length)])
 
@@ -38,6 +44,18 @@ const _getMessages = (addr) => (
     .then(hash => _fetchTransform(`/request/mail/id/${hash}`))
 )
 
+const _retryMail = (addr) => {
+    promiseRetry((retry, number) => {
+        return _getMessages(addr)
+            .then(res => {
+                if (!res[i]) {
+                    retry(res);
+                }
+                return res;
+            });
+    });
+}
+
 /**
  * Helper with queryable diespoable mailbox api for use in tests.
  */
@@ -54,33 +72,42 @@ class Mailbox extends Helper {
         }
     }
 
-    _init() {
-        this.createMailbox(null)
-    }
-
     createMailbox(x) {
-        return _createAddress(x).then(a => this.mailbox.address = a)
+        _createAddress(x).then(a => this.mailbox.address = a)
             .then(_getMessages)
             .then(m => this.mailbox.messages = m)
-    }
 
-    deleteMessage(x) {
-        return _deleteMessage(x)
-    }
-
-    getMessages(x = this.mailbox.address) {
-        return _getMessages(x).then(r => console.log(r))
-            .then(m => this.mailbox.messages = m)
-    }
-
-    getMailbox() {
-        console.log(this.mailbox)
         return this.mailbox
     }
 
-    getLatestMail(m = this.mailbox.messages) {
-        console.log(m)
-        return m != typeof Object ? m[m.length] : m.error
+    deleteMessage(x = this.mailbox.messages) {
+        !x[0].mail_id ? x = '' : x[0].mail_id
+        _deleteMessage(x)
+
+        return this.getMessages()
+    }
+
+    getMessages(x = this.mailbox.address) {
+        _getMessages(x)
+            .then(m => this.mailbox.messages = m)
+
+        return this.mailbox.messages
+    }
+
+    getMailbox() {
+        return this.mailbox
+    }
+
+    getLatestMail(x = this.mailbox) {
+        this.getMessages(x.address)
+        return x != typeof Object ? x.messages[x.messages.length - 1] : x.messages
+    }
+
+    waitForMail(x = this.mailbox.address) {
+        _retryMail(x)
+            .then(m => this.mailbox.messages = m)
+
+        return this.mailbox.messages
     }
 }
 
