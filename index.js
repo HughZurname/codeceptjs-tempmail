573696734ae64cadbfaee2984ff175fefdafff71
Craig de Gouveia <craig.degouveia@gmail.com>
2017-08-03 11:11:11 +0100

    made prettier

--- index.js
+++ index.js
@@ -7,107 +7,113 @@ const promiseRetry = require("promise-retry");
  */
 
 class Mailbox extends Helper {
-  /**
-    * 
+    /**
+    *
     * @param {Object} config configuration can be overridded by values found in `codecept.json`
     */
-  constructor(config) {
-    super(config);
-    this.mailbox = {
-      address: "",
-      messages: []
-    };
-    this.options = {};
-    Object.assign(this.options, config);
-  }
-  /**
-     * 
-     * @param {string=} name - Optional string to be used in mainlbox address  
+    constructor(config) {
+        super(config);
+        this.mailbox = {
+            address: "",
+            messages: []
+        };
+        this.options = {};
+        Object.assign(this.options, config);
+    }
+    /**
+     *
+     * @param {string=} name - Optional string to be used in mainlbox address
      * @returns {object} - Creates a mailbox object and checks for mail before returning.
-     * @example 
+     * @example
      * I.createMailbox('testmail') //{address: "testmail@doanart.com", messages: {error: "there are no messages yet"}}
      * I.createMailbox() //{address: "taij1q1j8n8@doanart.com", messages: {error: "there are no messages yet"}}
      */
-  createMailbox(name) {
-    const y = this.mailbox;
+    createMailbox(name) {
+        const y = this.mailbox;
 
-    _createAddress(name)
-      .then(a => (y.address = a))
-      .then(_getMessages)
-      .then(m => (y.messages = m));
+        _createAddress(name)
+            .then(a => (y.address = a))
+            .then(_getMessages)
+            .then(m => (y.messages = m));
 
-    return y;
-  }
+        return y;
+    }
 
-  /**
-    * 
+    /**
+    *
     * @param {object=} mailbox - Takes a mailbox object and detletes the last (i.e. most recent) value from messages and the mail server.
     */
-  deleteLatestMessage(mailbox = this.mailbox) {
-    if (mailbox.messages !== [] && !Array.isArray(mailbox.messages)) {
-      let y = mailbox.messages.pop();
-      return _deleteMessage(y.mail_id);
+    deleteLatestMessage(mailbox = this.mailbox) {
+        if (mailbox.messages !== [] && !Array.isArray(mailbox.messages)) {
+            let y = mailbox
+                .messages
+                .pop();
+            return _deleteMessage(y.mail_id);
+        }
+        return;
     }
-    return;
-  }
-  /**
-     * 
+    /**
+     *
      * @param {object=} mailbox - Takes a mailbox object and uses the address to get messages.
      */
-  getMessages(mailbox = this.mailbox) {
-    return _getMessages(mailbox.address)
-      .then(m => (mailbox.messages = m))
-      .catch(error => this.lastError);
-  }
-  /**
+    getMessages(mailbox = this.mailbox) {
+        return _getMessages(mailbox.address)
+            .then(m => (mailbox.messages = m))
+            .catch(error => this.lastError);
+    }
+    /**
      * @returns The mailbox object in it's current state, i.e. if you have called the methods elsewhere in this file without arguments, the Mailbox object is updated internally. This returns it.
      */
-  getMailbox() {
-    return this.mailbox;
-  }
-  /**
- * 
+    getMailbox() {
+        return this.mailbox;
+    }
+    /**
+ *
  * @param {*} mailbox {object=} mailbox - Takes a mailbox object and assigns a `mailbox.latest` object with the last (i.e. most recent) value in the messages array returned from the server.
- * 
+ *
  */
-  getLatestMessage(mailbox = this.mailbox) {
-    _getMessages(mailbox.address)
-      .then(m => (m != typeof "object" ? (mailbox.latest = m.pop()) : (mailbox.latest = m)))
-      .catch(error => this.lastError);
+    getLatestMessage(mailbox = this.mailbox) {
+        _getMessages(mailbox.address).then(m => (m != typeof "object"
+            ? (mailbox.latest = m.pop())
+            : (mailbox.latest = m))).catch(error => this.lastError);
 
-    return mailbox.latest;
-  }
-  /**
- * 
+        return mailbox.latest;
+    }
+    /**
+ *
  * @param {string} id - Takes a mail_id string and returns it from the `mailbox.messages` array
  */
-  getMailById(id) {
-    return this.mailbox.messages.filter(obj => {
-      return obj.mail_id == id;
-    });
-  }
-  /**
- * 
+    getMailById(id) {
+        return this
+            .mailbox
+            .messages
+            .filter(obj => {
+                return obj.mail_id == id;
+            });
+    }
+    /**
+ *
  * @param {object=} mailbox - Takes a mailbox object and makes 10 attempts at retrieving messages from the server. Once a suitible response is recieved the `mailbox.messages` and `mailbox.latest` are updated.
  */
-  waitForMessage(mailbox = this.mailbox) {
-    return promiseRetry(retry => {
-      return _getMessages(mailbox.address)
-        .then(res => {
-          if (!Array.isArray(res)) {
-            retry(res);
-          }
-          return (mailbox.messages = res);
-        })
-        .then(m => (mailbox.latest = m.pop()))
-        .catch(error => this.lastError);
-    });
-  }
+    waitForMessage(mailbox = this.mailbox) {
+        return promiseRetry(retry => {
+            return _getMessages(mailbox.address).then(res => {
+                if (!Array.isArray(res)) {
+                    retry(res);
+                }
+                return (mailbox.messages = res);
+            })
+                .then(m => (mailbox.latest = m.pop()))
+                .catch(error => this.lastError);
+        });
+    }
 }
 
 const apiUrl = "http://api.temp-mail.ru";
 
-const _hashAddress = str => new Promise((resolve, reject) => str ? resolve(crypto.createHash("md5").update(str).digest("hex")) : reject("No string provided"));
+const _hashAddress = str => new Promise((resolve, reject) => str
+    ? resolve(crypto.createHash("md5").update(str).digest("hex"))
+    : reject("No string provided"));
 
 const _transformResponse = res => res.json();
 
@@ -115,13 +121,18 @@ const _fetchTransform = path => fetch(`${apiUrl}${path || ""}/format/json/`).the
 
 const _randArrayItem = arr => arr[Math.floor(Math.random() * arr.length)];
 
-const _randString = () => Math.random().toString(32).substring(2);
+const _randString = () => Math
+    .random()
+    .toString(32)
+    .substring(2);
 
 const _genRandomMail = () => _fetchTransform("/request/domains/").then(r => _randString() + _randArrayItem(r));
 
 const _genNamedMail = str => _fetchTransform("/request/domains/").then(r => str + _randArrayItem(r));
 
-const _createAddress = name => (name ? _genNamedMail(name) : _genRandomMail());
+const _createAddress = name => (name
+    ? _genNamedMail(name)
+    : _genRandomMail());
 
 const _deleteMessage = msgId => _fetchTransform(`/request/delete/id/${msgId}`);
 
